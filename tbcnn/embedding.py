228e69daad9b7d14e46f4699d63fe8fab0fa3e53
Aetf <aetf@unlimitedcodeworks.xyz>
2017-04-10 23:09:45 -0400

    Embedding norm has no good effect

--- tbcnn/embedding.py
+++ tbcnn/embedding.py
@@ -200,8 +200,7 @@ def main():
     tree_sum = tree_sum_blk(l2loss_blk)
     compiler = td.Compiler.create(tree_sum)
     (batched_loss, ) = compiler.output_tensors
-    loss = tf.reduce_mean(batched_loss) + tf.reduce_mean(tf.subtract(tf.norm(param.get('We'), axis=1),
-                                                                     hyper.embedding_norm))
+    loss = tf.reduce_mean(batched_loss)
     opt = tf.train.AdamOptimizer(learning_rate=hyper.learning_rate)
 
     global_step = tf.Variable(0, trainable=False, name='global_step')
