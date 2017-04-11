c0458d9457d99dd809c34212ea68a66321b3a7cf
Aetf <aetf@unlimitedcodeworks.xyz>
2017-04-11 15:50:36 -0400

    Change default param

--- tbcnn/config.py
+++ tbcnn/config.py
@@ -21,8 +21,8 @@ class hyper(object):
     # structure
     use_relu = False
     # learning
-    learning_rate = 0.0002  # learning rate
-    weight_decay = 0.002
+    learning_rate = 0.00002  # learning rate
+    weight_decay = 0.0002
     batch_size = 128
     num_epochs = 50
     # directories
@@ -51,8 +51,8 @@ class hyper(object):
                                 help='directory for model checkpoints, defaults to WORK_DIR/model')
             parser.add_argument('--num_epochs', help='total number of epochs', type=int, default=50)
             parser.add_argument('--batch_size', help='batch size', type=int, default=128)
-            parser.add_argument('--learning_rate', help='learning rate', type=float, default=0.0002)
-            parser.add_argument('--weight_decay', help='weight decay factor', type=float, default=0.002)
+            parser.add_argument('--learning_rate', help='learning rate', type=float, default=0.00002)
+            parser.add_argument('--weight_decay', help='weight decay factor', type=float, default=0.0002)
             parser.add_argument('--word_dim', help='dimension of node feature', type=int, default=100)
             parser.add_argument('--conv_dim',
                                 help='dimension of conv feature detectors', type=int, default=50)
